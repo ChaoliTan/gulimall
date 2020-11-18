d03287534450a51a02c0907cb1de687f6c7a8726
ChaoliTan <ctan7749>
2020-11-17 23:16:12 -0800

    1. Add basic auth in 'GulimallElasticSearchConfig.java'; 2. Create 2 tests, one is index data to elasticsearch, and the other is search date from es.
    Signed-off-by: ChaoliTan <ctan7749>

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/config/GulimallElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -1,24 +1,55 @@
 package com.atguigu.gulimall.search.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 1. Import dependency
  * 2. Configure, Inject a "RestHighLevelClient" to bean
- * 3. Look up Official API
+ * 3. Look up Official API https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high.html
  */
 @Configuration
 public class GulimallElasticSearchConfig {
 
+    public static final RequestOptions COMMON_OPTIONS;
+    final CredentialsProvider credentialsProvider =
+            new BasicCredentialsProvider();
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
     public RestHighLevelClient esRestClient() {
+
+
+        credentialsProvider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials("elastic", "changeme"));
 
         RestClientBuilder builder = null;
         // String hostname, int port, String scheme
         builder = RestClient.builder(new HttpHost("localhost", 9200, "http"));
+        builder.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                                                @Override
+                                                public HttpAsyncClientBuilder customizeHttpClient(
+                                                        HttpAsyncClientBuilder httpClientBuilder) {
+                                                    return httpClientBuilder
+                                                            .setDefaultCredentialsProvider(credentialsProvider);
+                                                }
+                                            });
         RestHighLevelClient client = new RestHighLevelClient(builder);
 
 //        RestHighLevelClient client = new RestHighLevelClient(
--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,11 +1,32 @@
 package com.atguigu.gulimall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.gulimall.search.config.GulimallElasticSearchConfig;
+import lombok.Data;
+import lombok.ToString;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Avg;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,6 +34,112 @@ public class GulimallSearchApplicationTests {
 
     @Autowired
     private RestHighLevelClient client;
+
+    @ToString
+    @Data
+    static class Account {
+
+        private int account_number;
+        private int balance;
+        private String firstname;
+        private String lastname;
+        private int age;
+        private String gender;
+        private String address;
+        private String employer;
+        private String email;
+        private String city;
+        private String state;
+    }
+
+    @Test
+    public void searchData() throws IOException {
+        // 1. Create search request
+        SearchRequest searchRequest = new SearchRequest();
+        // Designate index
+        searchRequest.indices("bank");
+        // Designate DSL (Domain Specific Language), Search Conditions
+        // SearchSourceBuilder sourceBuilder Encapsulate condition
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        // 1.1 Create search conditions
+        sourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+//        sourceBuilder.from();
+//        sourceBuilder.size();
+//        sourceBuilder.aggregation();
+
+        // 1.2
+        TermsAggregationBuilder ageAgg = AggregationBuilders.terms("ageAgg").field("age").size(10);
+        sourceBuilder.aggregation(ageAgg);
+
+        // 1.3
+        AvgAggregationBuilder balanceAve = AggregationBuilders.avg("balanceAvg").field("balance");
+        sourceBuilder.aggregation(balanceAve);
+
+        searchRequest.source(sourceBuilder);
+
+        // 2. Execute search
+        SearchResponse searchResponse = client.search(searchRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+
+        // 3. Analyse searchResponse
+        System.out.println(searchResponse.toString());
+//        JSON.parseObject(searchResponse.toString(), Map.class);
+
+        // 3.1 Get all hits
+        SearchHits totalHits = searchResponse.getHits();
+        SearchHit[] hits = totalHits.getHits();
+        for (SearchHit hit : hits) {
+//            hit.getIndex();hit.getType();hit.getId();
+            String string = hit.getSourceAsString();
+            Account account = JSON.parseObject(string, Account.class);
+            System.out.println("account:"+account);
+        }
+
+        // 3.2 Get nested aggregation data
+        Aggregations aggregations = searchResponse.getAggregations();
+
+//        for (Aggregation aggregation : aggregations.asList()) {
+//            aggregation.getName();
+//            System.out.println("current aggregation's name: " + aggregation.getName());
+//        }
+        Terms ageAgg1 = aggregations.get("ageAgg");
+        for (Terms.Bucket bucket : ageAgg1.getBuckets()) {
+            String keyAsString = bucket.getKeyAsString();
+            System.out.println("age: " + keyAsString + "==>" + bucket.getDocCount());
+        }
+
+        Avg balanceAve1 = aggregations.get("balanceAvg");
+        System.out.println("Average salary: "+balanceAve1.getValue());
+    }
+    /**
+     * Test index data to es
+     * Update works as well
+     */
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+//        indexRequest.source("userName", "John", "age", "18", "gender", "M");
+        User user = new User();
+        user.setUserName("John");
+        user.setAge(18);
+        user.setGender("M");
+
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON); // Content to save
+
+        // Execute
+        IndexResponse index = client.index(indexRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+
+        // Extract response data
+        System.out.println(index);
+    }
+
+    @Data
+    class User {
+        private String userName;
+        private String gender;
+        private Integer age;
+    }
 
     @Test
     public void contextLoads() {
