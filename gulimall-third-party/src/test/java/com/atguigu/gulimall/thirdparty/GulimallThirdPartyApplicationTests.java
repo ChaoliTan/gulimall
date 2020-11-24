b19ca5bd964f6f042dd24542cdc189a80581d810
ChaoliTan <ctan7749>
2020-11-24 09:43:27 -0800

    1. Change configuration
    Signed-off-by: ChaoliTan <ctan7749>

--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -20,8 +20,8 @@ class GulimallThirdPartyApplicationTests {
 //        // Endpoint以杭州为例，其它Region请按实际情况填写。
 //        String endpoint = "oss-us-west-1.aliyuncs.com";
 //        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-//        String accessKeyId = "LTAI4G9on1ZPjy38eKMZJhDL";
-//        String accessKeySecret = "w1Yjdsv750WChXzZHfaPW3xnhSqlps";
+//        String accessKeyId = "";
+//        String accessKeySecret = "";
 //
 //        // 创建OSSClient实例。
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
