743983fe3e945f891cc6a6201a1332e32d71d5c6
ChaoliTan <ctan7749>
2020-11-24 19:52:08 -0800

    1. Update some html files 2. Update product mapping in elasticsearch 3. Add dsl.json Domain Specific Language
    Signed-off-by: ChaoliTan <ctan7749>

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/constant/EsConstant.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/constant/EsConstant.java
@@ -2,5 +2,5 @@ package com.atguigu.gulimall.search.constant;
 
 public class EsConstant {
 
-    public static final String PRODUT_INDEX = "product"; // sku数据在es中的索引
+    public static final String PRODUT_INDEX = "gulimall_product"; // sku数据在es中的索引
 }
