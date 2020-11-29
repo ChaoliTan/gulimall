47b02b937ee9c1a5c03acbdb848040db484d2aa6
ChaoliTan <ctan7749>
2020-11-29 13:20:17 -0800

    Add "gulimall-auth" service
    Signed-off-by: ChaoliTan <ctan7749>

---
+++ gulimall-auth/src/test/java/com/atguigu/gulimall/auth/GulimallAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
