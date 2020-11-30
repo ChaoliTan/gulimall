b143903200b98618531c0ad258b31b58bc01c45e
ChaoliTan <ctan7749>
2020-11-29 18:39:25 -0800

    Add ViewController in "gulimall-auth" service
    Signed-off-by: ChaoliTan <ctan7749>

--- gulimall-auth/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -1,21 +1,17 @@
 package com.atguigu.gulimall.auth.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
-    @GetMapping("/login.html")
-    public String loginPage(HttpSession session) {
-        return "login";
-    }
+//    @GetMapping("/login.html")
+//    public String loginPage(HttpSession session) {
+//        return "login";
+//    }
 
-
-    @GetMapping("/register.html")
-    public String registerPage(HttpSession session) {
-        return "register";
-    }
+//    @GetMapping("/register.html")
+//    public String registerPage(HttpSession session) {
+//        return "register";
+//    }
 
 }
